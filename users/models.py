f190d04eadaec1f95b55d025fe5d64b31bb2d27a
SUUNA_RAYMOND <raymondsuuna05@gmail.com>
2025-05-22 22:22:48 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -84,7 +84,7 @@ class Issue(models.Model):
         ('in_progress', 'In Progress'),
         ('resolved', 'Resolved'),
     ]
-    issue_id = models.CharField(max_length=20, unique=True, editable=False
+    issue_id = models.CharField(max_length=20, unique=True, editable=False)
     # the choices for the issue category
     CATEGORY_CHOICES = [
         ('missing_marks', 'Missing Marks'),
